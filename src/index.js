f5683abd1d6b4983f9b5e8c8584f2e8302a3604d
Michael Sisk <me@mgsisk.com>
2020-09-22 19:40:53 -0400

    Update index.js
    Refactored for PostCSS 8

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-'use strict'
-
 const modular = require('./modular.js')
 const normalize = require('./normalize.js')
-const postcss = require('postcss')
 
-module.exports = postcss.plugin('@mgsisk/postcss-modular-rhythm', (options)=> {
+module.exports = (options = {})=> { // eslint-disable-line import/group-exports
   const defaults = {
     bases: [1],
     fontSize: 1,
     fontUnit: 'em',
     lineHeight: 1.5,
+    lineMin: -1,
     normal: {},
     ratio: 1.2,
     rhythmUnit: 'rem',
@@ -18,49 +16,62 @@ module.exports = postcss.plugin('@mgsisk/postcss-modular-rhythm', (options)=> {
 
   options = {...defaults, ...options}
 
-  return (root)=> {
-    root.walkDecls((decl)=> {
-      if (decl.parent.selector !== ':root') {
-        return
+  return {
+    postcssPlugin: '@mgsisk/postcss-modular-rhythm',
+    Declaration: {
+      '--font-size': (decl)=> {
+        if (decl.parent.selector !== ':root') {
+          return
+        }
+
+        options.fontSize = parseFloat(decl.value)
+        options.fontUnit = decl.value.replace(/[-\d.]+/u, '')
+      },
+      '--line-height': (decl)=> {
+        if (decl.parent.selector !== ':root') {
+          return
+        }
+
+        options.lineHeight = parseFloat(decl.value)
+        options.rhythmUnit = decl.value.replace(/[-\d.]+/u, '')
+      },
+      '--modular-scale': (decl, {list})=> {
+        if (decl.parent.selector !== ':root') {
+          return
+        }
+
+        const [ratio, ...bases] = list.space(decl.value)
+
+        options.ratio = parseFloat(ratio)
+        options.bases = Array.from(new Set(bases)).map(value=> parseFloat(value)) // eslint-disable-line no-undef
+
+        if (!options.bases.length) {
+          options.bases.push(1)
+        }
+      },
+    },
+    RootExit(root) {
+      if (options.lineMin === -1) {
+        options.lineMin = options.ratio
       }
 
-      switch (decl.prop) {
-        case '--font-size':
-          options.fontSize = parseFloat(decl.value)
-          options.fontUnit = decl.value.replace(/[-\d.]+/u, '')
-          break
-        case '--line-height':
-          options.lineHeight = parseFloat(decl.value)
-          options.rhythmUnit = decl.value.replace(/[-\d.]+/u, '')
-          break
-        case '--modular-scale': {
-          const [ratio, ...bases] = postcss.list.space(decl.value)
+      options.normal.bases = normalize.bases(options.ratio, options.bases)
+      options.normal.fontSize = normalize.fontSize(options.fontSize, options.fontUnit, options.bases[0])
+      options.normal.lineHeight = normalize.lineHeight(
+        options.lineHeight,
+        options.rhythmUnit,
+        options.bases[0],
+        options.normal.fontSize,
+        options.fontUnit,
+      )
 
-          options.ratio = parseFloat(ratio)
-          options.bases = Array.from(new Set(bases)).map(value=> parseFloat(value)) // eslint-disable-line no-undef
-
-          if (!options.bases.length) {
-            options.bases.push(1)
-          }
-
-          break
-        } default: // Do nothing; this isn't a property the plugin handles.
-      }
-    })
-
-    options.normal.bases = normalize.bases(options.ratio, options.bases)
-    options.normal.fontSize = normalize.fontSize(options.fontSize, options.fontUnit, options.bases[0])
-    options.normal.lineHeight = normalize.lineHeight(
-      options.lineHeight,
-      options.rhythmUnit,
-      options.bases[0],
-      options.normal.fontSize,
-      options.fontUnit,
-    )
-
-    root.replaceValues(/(-?\d*\.?\d+)mfs\b/gu, value=> modular.fontSize(parseFloat(value), options))
-    root.replaceValues(/(-?\d*\.?\d+)mlh\b/gu, value=> modular.lineHeight(parseFloat(value), options))
-    root.replaceValues(/(-?\d*\.?\d+)msu\b/gu, value=> modular.scaleUnit(parseFloat(value), options))
-    root.replaceValues(/(-?\d*\.?\d+)vru\b/gu, value=> modular.rhythmUnit(parseFloat(value), options))
+      root.replaceValues(/(-?\d*\.?\d+)mfs\b/gu, value=> modular.fontSize(parseFloat(value), options))
+      root.replaceValues(/(-?\d*\.?\d+)mlh\b/gu, value=> modular.lineHeight(parseFloat(value), options).height)
+      root.replaceValues(/(-?\d*\.?\d+)msu\b/gu, value=> modular.scaleUnit(parseFloat(value), options))
+      root.replaceValues(/(-?\d*\.?\d+)vrl\b/gu, value=> modular.rhythmUnit(modular.lineHeight(parseFloat(value), options).multiplier, options))
+      root.replaceValues(/(-?\d*\.?\d+)vru\b/gu, value=> modular.rhythmUnit(parseFloat(value), options))
+      root.replaceValues(/(-?\d*\.?\d+)xlh\b/gu, value=> modular.lineHeight(parseFloat(value), options).multiplier)
+    },
   }
-})
+}
+module.exports.postcss = true // eslint-disable-line import/group-exports
