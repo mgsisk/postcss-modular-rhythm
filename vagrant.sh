24bf82e45de98f917129cb222e3629457f43ed18
Michael Sisk <me@mgsisk.com>
2020-09-24 16:29:44 -0400

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 hostname=$(grep -m 1 '\.test' /etc/hosts | sed 's/		*/ /g' | cut -d ' ' -f 2)
 locale=${1:-'en_US.UTF-8'}
 timezone=${2:-'UTC'}
-vnode='13'
+vnode='14'
 
 # NOTE Use the Node version specified in package.json, if any.
 if [ -e /vagrant/package.json ] && grep -q '"node":'; then
@@ -36,14 +36,8 @@ fi
 
 if [ ! -e /etc/apt/sources.list.d/vagrant.list ]; then
   printf 'Updating apt sources'
-  wget -qO - https://nginx.org/keys/nginx_signing.key | apt-key add - >/dev/null
-  printf 'deb http://nginx.org/packages/mainline/debian/ %s nginx\n' "$(lsb_release -cs)" >> /etc/apt/sources.list.d/vagrant.list
   wget -qO - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - >/dev/null
   printf 'deb https://deb.nodesource.com/node_%s.x %s main\n' "$vnode" "$(lsb_release -cs)" >> /etc/apt/sources.list.d/vagrant.list
-  wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add - >/dev/null
-  printf 'deb https://packages.sury.org/php %s main\n' "$(lsb_release -cs)" >> /etc/apt/sources.list.d/vagrant.list
-  wget -qO - wget http://www.webmin.com/jcameron-key.asc | apt-key add - >/dev/null
-  printf 'deb https://download.webmin.com/download/repository sarge contrib' >> /etc/apt/sources.list.d/vagrant.list
   apt-get -qq update
 fi
 
